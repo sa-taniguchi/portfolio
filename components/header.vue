d8e9fc4ee68c77ea098f766e39851b9237a1bed3
谷口聡 <taniguchisatoshi@taniguchisatoshinoMacBook-Air.local>
2023-11-13 09:44:23 +0900

    slight changed

--- components/header.vue
+++ components/header.vue
@@ -2,7 +2,7 @@
     <header id="header" :class="{'is-top': $route.path === '/'}" ref="headerRef">
         <div class="xl-container">
             <h1><nuxtLink to="/"><NuxtImg src="/img/common/logo.svg" alt="ポートフォリオ"></NuxtImg></nuxtLink></h1>
-            <button type="button" id="js-header-hmbg" class="hmbg_btn" aria-controls="js-header-nav">
+            <button type="button" id="js-header-hmbg" class="hmbg_btn" aria-controls="js-header-nav" aria-label="メニューを開く">
                 <span class="hmbg_bar"></span>
             </button>
 
