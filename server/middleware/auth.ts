967c966876aa9a6b3768b4f79957b4cc06aee200
sa-taniguchi <sa.taniguchi21@gmail.com>
2026-04-08 01:23:04 +0900

    modify: Basic認証スキップ

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -10,7 +10,7 @@ export default defineEventHandler((event) => {
 	const pass = config.basicAuthPassword;
 
 	// 環境変数がセットされていない場合は認証をスキップ
-	if (!user || !pass) return;
+	// if (!user || !pass) return;
 
 	const authHeader = getRequestHeader(event, 'authorization');
 	const expected = `Basic ${Buffer.from(`${user}:${pass}`).toString('base64')}`;
