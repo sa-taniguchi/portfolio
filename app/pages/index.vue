4cd9489484ab1022f0262966c5108e0233358a03
sa-taniguchi <sa.taniguchi21@gmail.com>
2026-04-07 21:26:55 +0900

    add: topページ

---
+++ app/pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useWorkStore } from '~~/stores/work';
+import { useSkillStore } from '~~/stores/skill';
+
+const skillStore = useSkillStore();
+const workStore = useWorkStore();
+
+const SectionTopAbout = defineAsyncComponent(() => import('~/components/section/TopAbout.vue'));
+const SectionTopSkill = defineAsyncComponent(() => import('~/components/section/TopSkill.vue'));
+const SectionTopWorks = defineAsyncComponent(() => import('~/components/section/TopWorks.vue'));
+
+onMounted(async () => {
+	await Promise.all([
+		!skillStore.skill ? skillStore.fetchSkill() : Promise.resolve(),
+		!workStore.works ? workStore.fetchWorks() : Promise.resolve(),
+	]);
+
+	console.log('両方のデータ取得（またはスキップ）が完了しました');
+});
+
+definePageMeta({
+	title: 'テスト',
+	description: 'テストディスクリプション',
+});
+</script>
+
+<template>
+	<div>
+		<SectionTopKv />
+		<SectionTopAbout />
+		<SectionTopSkill />
+		<SectionTopWorks />
+	</div>
+</template>
