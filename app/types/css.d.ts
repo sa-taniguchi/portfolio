db3a46cf727ebefd1f2ec6b0bee3509d7a9ac92b
sa-taniguchi <sa.taniguchi21@gmail.com>
2026-04-15 20:01:25 +0900

    modify: swiper tsconfig

---
+++ app/types/css.d.ts
@@ -0,0 +1,4 @@
+// CSS 副作用インポート用の型宣言
+// Swiper の CSS など、CSS を直接 import する場合に必要です
+
+declare module '*.css';
---
+++ css.d.ts
@@ -0,0 +1,5 @@
+// Swiper の CSS 副作用インポート用の型宣言
+// 直接 import するパスを TypeScript に認識させるため
+
+declare module 'swiper/css';
+declare module 'swiper/css/pagination';
